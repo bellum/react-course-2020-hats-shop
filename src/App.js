3e424075d5215918a634f0885124c7cba5838501
Andrii Bespalko <bellumm@gmail.com>
2020-09-17 18:57:52 +0300

    Adds Header component with the logo and links to shop and contact form pages

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import {HomePage} from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import {Route, Switch} from "react-router-dom";
+import Header from './components/header/header.component';
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch> {/* Switch stops on first matched route */}
                 <Route exact path='/' component={HomePage} />
                 <Route exact path='/shop' component={ShopPage} />
