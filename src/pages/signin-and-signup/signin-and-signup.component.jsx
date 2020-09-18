10db58c5341804b47f5eeac4468b56489423550c
Andrii Bespalko <bellumm@gmail.com>
2020-09-18 22:48:26 +0300

    Implements sign up using email and password using newly added Signup component

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './signin-and-signup.styles.scss';
+
 import Signin from '../../components/signin/signin.component';
+import Signup from '../../components/signup/signup.component';
 
 const SigninAndSignup = (props) => (
     <div className='sign-in-and-sign-up'>
         <Signin/>
+        <Signup/>
     </div>
 );
 
