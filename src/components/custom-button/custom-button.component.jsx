20087b6d94963de5bdc517fd8cc0d0cc6e16e049
Andrii Bespalko <bellumm@gmail.com>
2020-09-21 17:13:55 +0300

    Updates collection item component with "Add to cart" button displayed on hover

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`custom-button ${isGoogleSignIn ? 'google-sign-in': ''}`} {...otherProps}>
+const CustomButton = ({children, isGoogleSignIn, hasInvertedColors, ...otherProps}) => (
+    <button
+        className={`custom-button ${isGoogleSignIn ? 'google-sign-in': ''} ${hasInvertedColors ? 'inverted': ''}`}
+        {...otherProps}
+    >
         {children}
     </button>
 );
