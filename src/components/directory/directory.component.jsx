329cc5692ce413871179368a3b2cca79f04bbe21
Andrii Bespalko <bellumm@gmail.com>
2020-09-22 23:36:00 +0300

    Moves home directory items from component-level state to redux store under new directoryReducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,66 +1,27 @@
-import React from "react";
+import React from 'react';
+import {connect} from 'react-redux';
 
 import './directory.styles.scss';
-import MenuItem from "../menu-item/menu-item.component";
+import MenuItem from '../menu-item/menu-item.component';
+import {createStructuredSelector} from 'reselect';
+import {selectDirectoryItems} from '../../redux/directory/directory.selectors';
 
-class Directory extends React.Component {
-    constructor(props) {
-        super(props);
+const Directory = ({items}) => {
+    return (
+        <div className='directory-menu'>
+            {
+                items.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps}/>
+                ))
+            }
+        </div>
+    );
+};
 
-        this.state = {
-            items: [
-                {
-                    id: 1,
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    subtitle: 'SHOP NOW',
-                    linkUrl: 'hats',
-                },
-                {
-                    id: 2,
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    subtitle: 'SHOP NOW',
-                    linkUrl: '',
-                },
-                {
-                    id: 3,
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    subtitle: 'SHOP NOW',
-                    linkUrl: '',
-                },
-                {
-                    id: 4,
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    subtitle: 'SHOP NOW',
-                    linkUrl: '',
-                },
-                {
-                    id: 5,
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    subtitle: 'SHOP NOW',
-                    linkUrl: '',
-                },
-            ]
-        };
-    }
 
-    render() {
-        return (
-            <div className='directory-menu'>
-                {
-                    this.state.items.map(({id, ...otherSectionProps}) => (
-                        <MenuItem key={id} {...otherSectionProps}/>
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const mapStateToProps = createStructuredSelector({
+   items: selectDirectoryItems,
+});
 
-export default Directory;
+
+export default connect(mapStateToProps)(Directory);
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,51 @@
+
+const INITIAL_STATE = {
+    items: [
+        {
+            id: 1,
+            title: 'hats',
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            subtitle: 'SHOP NOW',
+            linkUrl: 'hats',
+        },
+        {
+            id: 2,
+            title: 'jackets',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            subtitle: 'SHOP NOW',
+            linkUrl: '',
+        },
+        {
+            id: 3,
+            title: 'sneakers',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            subtitle: 'SHOP NOW',
+            linkUrl: '',
+        },
+        {
+            id: 4,
+            title: 'womens',
+            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            size: 'large',
+            subtitle: 'SHOP NOW',
+            linkUrl: '',
+        },
+        {
+            id: 5,
+            title: 'mens',
+            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            size: 'large',
+            subtitle: 'SHOP NOW',
+            linkUrl: '',
+        },
+    ]
+};
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+export default directoryReducer;
