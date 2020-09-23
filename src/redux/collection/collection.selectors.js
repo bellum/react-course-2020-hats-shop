e9c46dd0df249f1c2a5c10ef1fb47e89d9f10db0
Andrii Bespalko <bellumm@gmail.com>
2020-09-23 23:00:50 +0300

    Converts array of collections to map of collections which simplifies lookup

--- src/redux/collection/collection.selectors.js
+++ src/redux/collection/collection.selectors.js
@@ -1,13 +1,5 @@
 import {createSelector} from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5,
-};
-
 const selectCollections = state => state.collection;
 
 export const selectCollectionItems = createSelector(
@@ -17,7 +9,5 @@ export const selectCollectionItems = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollectionItems],
-    collection_items => collection_items.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam],
-    ),
+    collection_items => collection_items[collectionUrlParam],
 );
